4e108c0c077667408959cceffcb6f3bf920621f0
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2024-08-22 21:41:42 +0100

    try fastify

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,39 @@ const port = process.env.PORT || 3000;
 
 import Fastify from "fastify";
 
-app.get("/express", (req, res) => {
-  res.send({
-    message: "Hello from Express!",
-  });
+// app.get("/express", (req, res) => {
+//   res.send({
+//     message: "Hello from Express!",
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Express Server is at http://localhost:${port}`);
+// });
+
+const fastify = Fastify(
+  {
+    logger: true,
+  }
+);
+
+fastify.get("/", async (req, res) => {
+  return {
+    message: "Hello from Fastify Home!",
+  };
 });
 
-app.listen(port, () => {
-  console.log(`Express Server is at http://localhost:${port}`);
+fastify.get("/fastify", async (req, res) => {
+  return {
+    message: "Hello from Fastify endpoint!",
+  };
+});
+
+fastify.listen({ port: Number(port) }, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+    fastify.log.info(`server listening on ${address}`);
 });
