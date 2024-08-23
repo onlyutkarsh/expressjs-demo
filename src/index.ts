1c4ed4546b968b4e9531fac6545efbd85977c2db
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2024-08-23 07:00:20 +0100

    revert to express

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,33 @@ const port = process.env.PORT || 3000;
 
 import Fastify from "fastify";
 
-// app.get("/express", (req, res) => {
-//   res.send({
-//     message: "Hello from Express!",
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Express Server is at http://localhost:${port}`);
-// });
-
-const fastify = Fastify(
-  {
-    logger: true,
-  }
-);
-
-fastify.get("/fastify", async (req, res) => {
-  return {
-    message: "Hello from Fastify endpoint!",
-  };
+app.get("/express", (req, res) => {
+  res.send({
+    message: "Hello from Express!",
+  });
 });
 
-fastify.listen({ port: Number(port) }, function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-    // Server is now listening on ${address}
-    fastify.log.info(`server listening on ${address}`);
+app.listen(port, () => {
+  console.log(`Express Server is at http://localhost:${port}`);
 });
+
+// const fastify = Fastify(
+//   {
+//     logger: true,
+//   }
+// );
+
+// fastify.get("/fastify", async (req, res) => {
+//   return {
+//     message: "Hello from Fastify endpoint!",
+//   };
+// });
+
+// fastify.listen({ port: Number(port) }, function (err, address) {
+//     if (err) {
+//         fastify.log.error(err)
+//         process.exit(1)
+//     }
+//     // Server is now listening on ${address}
+//     fastify.log.info(`server listening on ${address}`);
+// });
